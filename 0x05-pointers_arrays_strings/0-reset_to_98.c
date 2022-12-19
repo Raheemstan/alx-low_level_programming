70d9fb27a09ccaf4e3d088bf315702a85931d0b1
Michael Raheem <abiolar544@gmail.com>
2022-12-19 15:48:29 -0800

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,14 +3,12 @@
 /**
  * reset_to_98: function to reset param
  * @n: int
- * Description: A function to reset the value of the parameter
- * n to 98 
  * 
  * Return: void
- *
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
