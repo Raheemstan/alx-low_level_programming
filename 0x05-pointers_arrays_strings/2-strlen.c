4725dd8f64a62314389a105c92085c62c88756b6
Michael Raheem <abiolar544@gmail.com>
2022-12-20 08:37:04 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - count the number of xters in a string
+ * @s: string
+ * Return: number of xters
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return len;
+}
+
