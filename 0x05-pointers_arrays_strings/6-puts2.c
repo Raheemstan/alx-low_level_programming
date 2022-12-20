b3824e9e1a5e2e649ecc93f7245832ab1dee2ec8
Michael Raheem <abiolar544@gmail.com>
2022-12-20 13:52:19 -0800

    put string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - print other xters with the first
+ * str: string text
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
