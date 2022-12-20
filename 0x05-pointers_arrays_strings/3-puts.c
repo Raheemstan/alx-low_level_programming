3537c5be8952608590d1014fee479457427599d8
Michael Raheem <abiolar544@gmail.com>
2022-12-20 08:54:49 -0800

    put line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - print a string and add a new line
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
