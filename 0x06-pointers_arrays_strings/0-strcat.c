8b461c0411efeecaf8a41170e86dc285754dc7a1
Michael Raheem <abiolar544@gmail.com>
2022-12-23 11:53:26 -0800

    0-hack

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,32 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _strcat - concatenate strings
- *
- * @dest: begin new string
- * @src: end new string
- *
- * Return: new string
- *
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i=0;
-	int j=0;
+	int i;
+	int j;
 
-	while (dest[i] && dest[i] !='\0')
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
 		i++;
-	while (src[j])
+	}
+
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
 		j++;
+		i++;
 	}
 
 	dest[i] = '\0';
-	
 	return (dest);
 }
-
