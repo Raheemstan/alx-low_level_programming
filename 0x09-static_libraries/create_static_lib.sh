4103be3d77c30d6f831039cc60a116c842ddc718
Michael Raheem <abiolar544@gmail.com>
2023-01-11 09:24:10 -0800

    retry again

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Compile all the .c files in the current directory into object files
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-
-# Create a static library called liball.a from the object files
+# create  a static library called liball.a from the object files
 ar -rcs liball.a *.o
-
-# Print the contents of the library to the terminal
-ar -t liball.a
