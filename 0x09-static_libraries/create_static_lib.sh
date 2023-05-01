01b18cdc06e0415edad40d62cb703bdd536b19d4
Michael Raheem <abiolar544@gmail.com>
2023-05-01 05:11:37 +0100

    better static

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
+
+# Create a static library called liball.a from all the .c files in the current directory
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+ar rc liball.a *.o
 ranlib liball.a
