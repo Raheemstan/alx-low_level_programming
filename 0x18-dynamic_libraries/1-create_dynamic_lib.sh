9d07b3b47d9a61d4874a17562c0e122d23698481
Michael Raheem <abiolar544@gmail.com>
2023-08-25 15:57:50 +0100

    adj

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
+
+gcc -c -Wall -Werror -fpic *.c
+
+gcc -shared -o liball.so *.o
+
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+
+rm *.o
